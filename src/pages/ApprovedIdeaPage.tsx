772a508cc78f14ae0e5808251b06fd098c976930
Nota0515 <businessbro78@gmail.com>
2025-07-21 22:15:44 +0530

    adjusted the love icon

--- src/pages/ApprovedIdeaPage.tsx
+++ src/pages/ApprovedIdeaPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { PiHeartThin } from "react-icons/pi";
 import { ArrowLeft, CheckCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -130,8 +131,11 @@ const ApprovedIdeaPage = () => {
           />
         </div>
         <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
-          <p className="text-2xl md:text-4xl font-feasy text-white">
-            made with 💖 by Akhilesh
+          <p className="text-2xl flex flex-row md:text-4xl font-feasy text-white">
+            made with&nbsp;<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg>
+            &nbsp;by Akhilesh
           </p>
         </div>
       </footer>
